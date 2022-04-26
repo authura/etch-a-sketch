28dbca883be103486aadf7e9e8a4a5c58467df8d
authura <rhoferdevs@gmail.com>
2022-04-26 10:44:34 -0400

    added function to change size of grid

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 
 
-const generateGrid = () => {
+const generateGrid = (size) => {
     const grid = document.querySelector('#grid');
-    grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
-    grid.style.gridTemplateRows = 'repeat(16, 1fr)';
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
 
     //generate grid squares
     for (let i = 0; i < 256; i++) {
         let square = document.createElement('div');
+        square.classList.add('grid-square');
         //blue to make visible for now
         square.style.backgroundColor = 'blue';
         grid.insertAdjacentElement("beforeend", square);
         
     }
+}
+
+generateGrid(16);
+
+const changeSize = (input) => {
+    generateGrid(input);
 }
