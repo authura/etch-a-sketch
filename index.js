b0860e18dd374b9aeec05d47c1243119e0f392a5
authura <rhoferdevs@gmail.com>
2022-04-26 11:06:09 -0400

    added event handler when hovering over grid squares that changes background color to black

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const generateGrid = (size) => {
     const grid = document.querySelector('#grid');
+    //remove divs when size input is changed
     let squares = grid.querySelectorAll('div')
     squares.forEach((div) => div.remove());
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
@@ -12,6 +13,9 @@ const generateGrid = (size) => {
     let amount = size * size;
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = 'black';
+        })
         //blue to make visible for now
         square.style.backgroundColor = 'blue';
         grid.insertAdjacentElement("beforeend", square);
